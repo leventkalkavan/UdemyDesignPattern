84f4afab4ece1a305d885cecda1e9418b3d9297f
leventkalkavan <levent.kalkavan@hotmail.com>
2023-08-29 16:17:38 +0300

    Base Identity Proje

--- BaseProject/Context/AppDbContext.cs
+++ BaseProject/Context/AppDbContext.cs
@@ -1,9 +1,10 @@
+using BaseProject.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BaseProject.Context;
 
-public class AppDbContext: IdentityDbContext
+public class AppDbContext: IdentityDbContext<AppUser>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 }
--- BaseProject/Program.cs
+++ BaseProject/Program.cs
@@ -7,16 +7,36 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("Mssql"));
 });
+
 builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
 {
-
     options.User.RequireUniqueEmail = true;
 }).AddEntityFrameworkStores<AppDbContext>();
+
 var app = builder.Build();
+
+using (var scope= app.Services.CreateScope()){
+
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+var userManager = scope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+dbContext.Database.Migrate();
+
+if (!userManager.Users.Any())
+{
+                                                                                            //passwordda hassasiyete dikkat
+    userManager.CreateAsync(new AppUser(){ UserName = "levent",Email = "levent@gmail.com"}, "P123s.").Wait();
+    userManager.CreateAsync(new AppUser(){ UserName = "kalkavan",Email = "kalkavan@hotmail.com"}, "P123s.").Wait();
+    userManager.CreateAsync(new AppUser(){ UserName = "marmaris",Email = "marmaris@hotmail.com"}, "P123s.").Wait();
+    userManager.CreateAsync(new AppUser(){ UserName = "test",Email = "test@outlook.com"}, "P123s.").Wait();
+}
+
+}
 
 if (!app.Environment.IsDevelopment())
 {
