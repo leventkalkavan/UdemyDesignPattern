0282f5ad994a6cc1594df1d297621b54a2c82b51
leventkalkavan <levent.kalkavan@hotmail.com>
2023-08-29 12:25:20 +0300

    update

--- BaseProject/Context/AppDbContext.cs
+++ BaseProject/Context/AppDbContext.cs
@@ -1,6 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 namespace BaseProject.Context;
 
-public class AppDbContext
+public class AppDbContext: IdentityDbContext
 {
-    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 }
+ 
